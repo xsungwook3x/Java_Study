b8c5603987212346476c091d46cb39de962f3269
Seonguk <xsungwook3x@naver.com>
2022-04-03 17:37:50 +0900

    기초코딩테스트 소수

---
+++ CodingTest/소수.java
@@ -0,0 +1,24 @@
+package CodingTest;
+import java.util.*;
+
+public class 소수 {
+
+    public int solution(int n){
+        int cnt=0;
+        int[] ch = new int[n+1];
+        for(int i=2; i<=n; i++){
+            if(ch[i]==0){
+                cnt++;
+                for(int j=i; j<=n; j=j+i) ch[j]=1;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args){
+        소수 T = new 소수();
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
